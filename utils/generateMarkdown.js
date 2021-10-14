eda1358aca134fe41065445fe02d7485557836d7
jack <jackmanzer88@gmail.com>
2021-10-14 14:52:02 -0500

    readme generator finished

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -10,14 +10,17 @@ function assignLicenseLink(license) {
   let licenseLink;
 
   switch(license){
-    case 'Apache License 2.0': 
-      licenseLink = "![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)"
-      break;
-    case 'MIT License':
+    case 'MIT':
       licenseLink = "![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)"
       break;
-    case 'Creative Commons Zero v1.0 Universal': 
-      licenseLink = "![License: CC0-1.0](https://img.shields.io/badge/License-CC0%201.0-lightgrey.svg)"
+    case 'APACHE 2.0': 
+      licenseLink = "![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)"
+      break;
+    case 'GPL 3.0': 
+      licenseLink = "![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)"
+      break;
+    case 'BSD 3': 
+      licenseLink = "![License](https://img.shields.io/badge/License-BSD%203--Clause-blue.svg)](https://opensource.org/licenses/BSD-3-Clause)"
       break;
     default: 
       licenseLink = '';    
@@ -37,18 +40,35 @@ function generateMarkdown(data) {
   const licenseBadge = assignLicenseLink(data.license);
 
   return `# ${data.title}\n` +
+        `${licenseBadge}\n` + 
         `## Description\n` +
         `${data.description}\n` +
+        `## Table of Contents\n` +
+        `* [Installation](#installation)\n` + 
+        `\n` +
+        `* [Usage](#usage)\n` + 
+        `* [License](#license)\n` + 
+        `* [Contributing](#contributing)\n` + 
+        `* [Tests](#tests)\n` + 
+        `* [Questions](#questions)\n` + 
+        `\n` +
         `## Installation\n` +
-        `${data.installation}\n` +
+        `    ${data.installation}\n` +
+        `\n` +
         `## Usage\n` +
         `${data.usageInformation}\n` +
+        `\n` +
+        `## License\n` +
+        `${licenseBadge}\n` +
+        `\n` +
         `## Contributing\n` +
         `${data.contributionGuidelines}\n` +
+        `\n` +
         `## Tests\n` +
-        `${data.testInstructions}\n` +
-        `## License\n` +
-        `${licenseBadge}\n`
+        `    ${data.testInstructions}\n` +
+        `\n` +
+        `## Questions\n` + 
+        `If you have any questions about the repo, open an issue or contact medirectly at ${data.email}. You can find more of my work by following this [link](https://github.com/${data.userName}) to my GitHub.`
 }
 
 module.exports = generateMarkdown;
